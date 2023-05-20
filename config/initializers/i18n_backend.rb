63718ea0b26d01eaa3474bc09eebc245d32c3608
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-05-20 11:32:49 +0200

    fix: error on initialization when the database is not present

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -4,15 +4,22 @@ class I18n::Backend::ActiveRecord::Translation
   self.table_name = 'cmor_translations_translations'
 end
 
-if I18n::Backend::ActiveRecord::Translation.table_exists?
-  backends = [I18n::Backend::ActiveRecord.new, I18n.backend]
+print "[Cmor::Translations] Adding I18n::Backend::ActiveRecord to the I18n::Backend::Chain"
+begin
+  if I18n::Backend::ActiveRecord::Translation.table_exists?
+    backends = [I18n::Backend::ActiveRecord.new, I18n.backend]
 
-  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
+    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
+    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
 
-  if Cmor::Translations::Configuration.prefer_database_translations
-    I18n.backend = I18n::Backend::Chain.new(*backends)
-  else
-    I18n.backend = I18n::Backend::Chain.new(*backends.reverse)
+    if Cmor::Translations::Configuration.prefer_database_translations
+      I18n.backend = I18n::Backend::Chain.new(*backends)
+    else
+      I18n.backend = I18n::Backend::Chain.new(*backends.reverse)
+    end
   end
+rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad
+  puts " => [WARNING] The database is not available"
+else
+  puts " => [OK]"
 end
