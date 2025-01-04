46d1eab407714a21b606adad327ca215fcf37726
Roberto Vasquez-Angel <rva@beegoodit.de>
2025-01-04 19:47:02 +0100

    fix: rails boot failing when database connection is not available

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,25 +1,27 @@
-require 'i18n/backend/active_record'
+ActiveSupport.on_load(:active_record) do
+  require 'i18n/backend/active_record'
 
-class I18n::Backend::ActiveRecord::Translation
-  self.table_name = 'cmor_translations_translations'
-end
-
-print "[Cmor::Translations] Adding I18n::Backend::ActiveRecord to the I18n::Backend::Chain"
-begin
-  if I18n::Backend::ActiveRecord::Translation.table_exists?
-    backends = [I18n::Backend::ActiveRecord.new, I18n.backend]
-
-    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
-
-    if Cmor::Translations::Configuration.prefer_database_translations
-      I18n.backend = I18n::Backend::Chain.new(*backends)
-    else
-      I18n.backend = I18n::Backend::Chain.new(*backends.reverse)
-    end
+  class I18n::Backend::ActiveRecord::Translation
+    self.table_name = 'cmor_translations_translations'
   end
-rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad
-  puts " => [WARNING] The database is not available"
-else
-  puts " => [OK]"
+
+  print "[Cmor::Translations] Adding I18n::Backend::ActiveRecord to the I18n::Backend::Chain"
+  begin
+    if I18n::Backend::ActiveRecord::Translation.table_exists?
+      backends = [I18n::Backend::ActiveRecord.new, I18n.backend]
+
+      I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
+      I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
+
+      if Cmor::Translations::Configuration.prefer_database_translations
+        I18n.backend = I18n::Backend::Chain.new(*backends)
+      else
+        I18n.backend = I18n::Backend::Chain.new(*backends.reverse)
+      end
+    end
+  rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad
+    puts " => [WARNING] Could not add I18n::Backend::ActiveRecord to the I18n::Backend::Chain: #{e.message}"
+  else
+    puts " => [OK]"
+  end
 end
